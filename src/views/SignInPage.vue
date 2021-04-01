8c03490b9c215cace7cb41cb11c6afca6e80f37f
Evgeniy <evgpashko92@gmail.com>
2021-04-01 16:16:20 +0700

    mapState
    Using mapState form vuex, refactoring code

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -42,6 +42,7 @@
 </template>
 
 <script>
+import {mapState} from "vuex";
 import ValidationErrors from "../components/ValidationErrors.vue";
 import {actionTypes} from "../store/modules/auth.js";
 
@@ -57,12 +58,16 @@ export default {
     };
   },
   computed: {
-    isSubmitting() {
-      return this.$store.state.auth.isSubmitting;
-    },
-    validationErrors() {
-      return this.$store.state.auth.validationErrors;
-    },
+    ...mapState({
+      isSubmitting: (state) => state.auth.isSubmitting,
+      validationErrors: (state) => state.auth.validationErrors,
+    }),
+    // isSubmitting() {
+    //   return this.$store.state.auth.isSubmitting;
+    // },
+    // validationErrors() {
+    //   return this.$store.state.auth.validationErrors;
+    // },
   },
   methods: {
     onSubmit() {
