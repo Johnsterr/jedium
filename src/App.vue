2c29b591e93dab7e50344cf59a48c624425ea908
Evgeniy <evgpashko92@gmail.com>
2021-04-01 19:40:31 +0700

    NavBar
    Create NavBar component, add navigation links and add "v-if" some logic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/signup">Sign Up</router-link>
-    </div>
+    <nav-bar></nav-bar>
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <router-link class="navbar-brand" :to="{name: 'home'}"
+        >Jedium</router-link
+      >
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            :to="{name: 'home'}"
+            active-class="active"
+            exact
+            >Home</router-link
+          >
+        </li>
+        <!-- Template for Logged User -->
+        <template v-if="isLoggedIn">
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{name: 'createArticle'}"
+              active-class="active"
+              exact
+              ><i class="ion-compose"></i> &nbsp; New Article</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{name: 'settings'}"
+              active-class="active"
+              exact
+              ><i class="ion-gear-a"></i> &nbsp; Settings</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{name: 'userProfile', params: {slug: currentUser.username}}"
+              active-class="active"
+              exact
+              ><img class="user-pic" :src="currentUser.image" alt="user_logo" />
+              &nbsp; {{ currentUser.username }}</router-link
+            >
+          </li>
+        </template>
+        <!-- Template for Not logged User -->
+        <template v-if="!isLoggedIn">
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{name: 'signin'}"
+              active-class="active"
+              exact
+              >Sign In</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{name: 'signup'}"
+              active-class="active"
+              exact
+              >Sign Up</router-link
+            >
+          </li>
+        </template>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: "NavBar",
+  computed: {
+    ...mapState({
+      currentUser: (state) => state.auth.currentUser,
+      isLoggedIn: (state) => state.auth.isLoggedIn,
+    }),
+  },
+};
+</script>
