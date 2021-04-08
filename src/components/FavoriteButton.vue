8ab0179958a502a6fd6efe7fc15593de0a16c433
Evgeniy <evgpashko92@gmail.com>
2021-04-08 23:44:06 +0700

    Favorites Button
    Add dynamic styles, complete functional

--- src/components/FavoriteButton.vue
+++ src/components/FavoriteButton.vue
@@ -1,11 +1,20 @@
 <template>
-  <button class="btn btn-sm btn-outline-primary">
-    <i class="ion-heart"></i> &nbsp;
-    <span class="counter">{{ article.favoritesCount }}</span>
+  <button
+    @click="handleLike"
+    class="btn btn-sm"
+    :class="{
+      'btn-primary': isFavoritedOptimistic,
+      'btn-outline-primary': !isFavoritedOptimistic,
+    }"
+  >
+    <i class="ion-heart"></i>
+    <span class="counter">&nbsp;{{ favoritesCountOptimistic }}</span>
   </button>
 </template>
 
 <script>
+import {actionTypes} from "../store/modules/favorites.js";
+
 export default {
   name: "FavoriteButton",
   props: {
@@ -20,6 +29,26 @@ export default {
     favoritesCount: {
       type: Number,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isFavoritedOptimistic: this.isFavorited,
+      favoritesCountOptimistic: this.favoritesCount,
+    };
+  },
+  methods: {
+    handleLike() {
+      this.$store.dispatch(actionTypes.addToFavorites, {
+        slug: this.articleSlug,
+        isFavorited: this.isFavorited,
+      });
+      if (this.isFavoritedOptimistic) {
+        this.favoritesCountOptimistic -= 1;
+      } else {
+        this.favoritesCountOptimistic += 1;
+      }
+      this.isFavoritedOptimistic = !this.isFavoritedOptimistic;
     },
   },
 };
